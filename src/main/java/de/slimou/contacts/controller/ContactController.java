a76a2b20ba82ba014164d43376db1682d18b696d
Salim Oussayfi <box@slimou.net>
2021-01-17 16:44:39 +0100

    added detailview template

--- src/main/java/de/slimou/contacts/controller/ContactController.java
+++ src/main/java/de/slimou/contacts/controller/ContactController.java
@@ -4,6 +4,8 @@ import de.slimou.contacts.repository.ContactRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactController {
@@ -23,5 +25,12 @@ public class ContactController {
     @GetMapping(path = "/new-contact")
     public String hinzufuegen(){
         return "contacts/contact-add";
+    }
+
+    @RequestMapping("/contact-detail")
+    public String detailansicht(@RequestParam("id") int id, Model model) {
+        model.addAttribute("contact_forname", contactRepository.findById(id).get().getForname());
+        model.addAttribute("contact_lastname", contactRepository.findById(id).get().getLastname());
+        return "contacts/contact-detail";
     }
 }
